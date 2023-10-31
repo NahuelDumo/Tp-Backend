0e17ce3205d15eae2a0d4a6e5d0e02281602987e
Nahuel Agustin Dumo <nahueldumo2014@gmail.com>
2023-10-31 17:55:46 -0300

    Modificacion de properties

--- alquileres_api/src/main/java/utn/frc/utn/edu/ar/alquileres_api/AlquileresApiApplication.java
+++ alquileres_api/src/main/java/utn/frc/utn/edu/ar/alquileres_api/AlquileresApiApplication.java
@@ -2,6 +2,7 @@ package utn.frc.utn.edu.ar.alquileres_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.format.annotation.DateTimeFormat;
 
 @SpringBootApplication
 public class AlquileresApiApplication {
@@ -9,5 +10,6 @@ public class AlquileresApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AlquileresApiApplication.class, args);
 	}
+
 
 }
